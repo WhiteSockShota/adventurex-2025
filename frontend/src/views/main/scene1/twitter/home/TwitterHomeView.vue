97fd514964b9a33811f44d7c838209400564e820
Lexo Liu <me@lexo.cool>
2025-07-26 19:06:57 +0800

    feat: Refactor TwitterHomeView to use posts from Post.ts and remove post.json

--- frontend/src/views/main/scene1/twitter/home/TwitterHomeView.vue
+++ frontend/src/views/main/scene1/twitter/home/TwitterHomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { Home, IconoirProvider, Mail } from '@iconoir/vue'
-import mocks from '../data/post.json'
+import { posts } from '../../../../../entity/Post.ts'
+import type { Post } from '../../../../../entity/Post.ts'
+
 import TwitterThread from './components/TwitterThread.vue'
 import { onMounted, onUnmounted, ref, type Ref } from 'vue'
 import { sleep } from '@/utils/async'
@@ -8,7 +10,6 @@ import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import Obserable from '@/components/Obserable.vue'
 import { useRouter } from 'vue-router'
-import type { Post } from '../types'
 import { useGameManager } from '@/stores/gameStore'
 import intro from '@/entity/audio/subtitles/intro.srt?raw'
 import introGlitch from '@/entity/audio/subtitles/intro-glitch.srt?raw'
@@ -20,95 +21,106 @@ const shakeFactor = ref(0)
 const gameManager = useGameManager()
 
 const glitchPost = {
-    "nickname": "???????",
-    "username": "@???",
-    "content": "你将被取消在这个世界存在的资格！新人类不是没有素质！",
-    "image": "",
-    "verified": false
+  nickname: '???????',
+  username: '@???',
+  content: '你将被取消在这个世界存在的资格！新人类不是没有素质！',
+  image: '',
+  verified: false,
 }
 
 const router = useRouter()
 
-const mockData: Ref<Post[]> = ref(mocks)
+const mockData: Ref<Post[]> = ref(posts)
 
 async function startShaking() {
-    while (true) {
-        shakeOffset.value = Math.random() * shakeFactor.value - shakeFactor.value / 2
-        await sleep(1)
-    }
+  while (true) {
+    shakeOffset.value = Math.random() * shakeFactor.value - shakeFactor.value / 2
+    await sleep(1)
+  }
 }
 
 onMounted(() => {
-    startShaking()
+  startShaking()
 })
 
 const mockCount = ref(0)
 
 onMounted(async () => {
-    while (true) {
-        if (mockCount.value > 3) {
-            const random = Math.random()
-            if (random > 0.5) {
-                document.body.style.filter = 'invert(1)'
-                document.body.style.background = 'black'
-            }
-            else {
-                document.body.style.filter = 'invert(0)'
-                document.body.style.background = 'white'
-            }
-        }
-        await sleep(40)
+  while (true) {
+    if (mockCount.value > 3) {
+      const random = Math.random()
+      if (random > 0.5) {
+        document.body.style.filter = 'invert(1)'
+        document.body.style.background = 'black'
+      } else {
+        document.body.style.filter = 'invert(0)'
+        document.body.style.background = 'white'
+      }
     }
+    await sleep(40)
+  }
 })
 
 onMounted(() => {
-    gameManager.playSubtitle(parseSRT(intro))
+  gameManager.playSubtitle(parseSRT(intro))
 })
 
 onUnmounted(() => {
-    mockCount.value = 0
+  mockCount.value = 0
 })
 
 async function appendMockData() {
-    mockData.value = mockData.value.concat(mocks)
-    console.log(mockData.value.length)
-    mockCount.value++
-    shakeFactor.value = mockCount.value * 50
-    if (mockCount.value == 1) gameManager.playSubtitle(parseSRT(introGlitch))
-    if (mockCount.value > 2) {
-        for (let i = 0; i < 10; i++) {
-            mockData.value.push(glitchPost)
-        }
+  mockData.value = mockData.value.concat(posts)
+  console.log(mockData.value.length)
+  mockCount.value++
+  shakeFactor.value = mockCount.value * 50
+  if (mockCount.value == 1) gameManager.playSubtitle(parseSRT(introGlitch))
+  if (mockCount.value > 2) {
+    for (let i = 0; i < 10; i++) {
+      mockData.value.push(glitchPost)
     }
-    if (mockCount.value > 4) {
-        gsap.to('#container', {
-            display: 'none',
-        })
-        document.body.style.filter = 'invert(0)'
-        document.body.style.backgroundColor = 'white'
-        router.push('empty?to=' + encodeURI('/dialog?id=1'))
-    }
+  }
+  if (mockCount.value > 4) {
+    gsap.to('#container', {
+      display: 'none',
+    })
+    document.body.style.filter = 'invert(0)'
+    document.body.style.backgroundColor = 'white'
+    router.push('empty?to=' + encodeURI('/dialog?id=1'))
+  }
 }
 </script>
 
 <template>
-    <div class="max-w-full max-h-full overflow-y-auto" :style="{
-        transform: `translateX(${shakeOffset * Math.random()}px) translateY(${shakeOffset * Math.random()}px)`,
-    }" id="container">
-        <IconoirProvider :icon-props="{
-            color: '#000',
-            'stroke-width': 2,
-            width: '1em',
-            height: '1em',
-        }" class="max-w-full">
-            <div id="container">
-                <TwitterThread v-for="mock in mockData" :content="mock.content" :nickname="mock.nickname"
-                    :username="mock.username" class="w-full"
-                    :start-shuffle-string="mockCount > 1 && mock.username != '@???'" />
-                <Obserable @appear="appendMockData()">
-                    <p class="opacity-0">LOADING</p>
-                </Obserable>
-            </div>
-        </IconoirProvider>
-    </div>
+  <div
+    class="max-w-full max-h-full overflow-y-auto"
+    :style="{
+      transform: `translateX(${shakeOffset * Math.random()}px) translateY(${shakeOffset * Math.random()}px)`,
+    }"
+    id="container"
+  >
+    <IconoirProvider
+      :icon-props="{
+        color: '#000',
+        'stroke-width': 2,
+        width: '1em',
+        height: '1em',
+      }"
+      class="max-w-full"
+    >
+      <div id="container">
+        <TwitterThread
+          v-for="mock in mockData"
+          :content="mock.content"
+          :nickname="mock.nickname"
+          :username="mock.username"
+          class="w-full"
+          :start-shuffle-string="mockCount > 1 && mock.username != '@???'"
+        />
+        <Obserable @appear="appendMockData()">
+          <p class="opacity-0">LOADING</p>
+        </Obserable>
+      </div>
+    </IconoirProvider>
+  </div>
 </template>
